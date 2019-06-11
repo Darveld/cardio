1e3cbfff8600c967202cea6999ac3ab2620a2cb1
Aleksandr <aleksandr.s@volpis.com>
2019-06-11 21:51:33 +0300

    global

--- app/src/main/java/com/valsoft/cardiodiary/CardioApp.java
+++ app/src/main/java/com/valsoft/cardiodiary/CardioApp.java
@@ -1,14 +1,11 @@
 package com.valsoft.cardiodiary;
 
 import android.app.Application;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 
 import com.valsoft.cardiodiary.di.AppComponent;
 import com.valsoft.cardiodiary.di.AppModule;
 import com.valsoft.cardiodiary.di.DaggerAppComponent;
-import com.valsoft.cardiodiary.presentation.ui.quality.QualityService;
 
 
 public class CardioApp extends Application {
--- app/src/main/java/com/valsoft/cardiodiary/di/quality/get/GetQualityComponent.java
+++ app/src/main/java/com/valsoft/cardiodiary/di/quality/get/GetQualityComponent.java
@@ -2,7 +2,6 @@ package com.valsoft.cardiodiary.di.quality.get;
 
 import com.valsoft.cardiodiary.data.repository.datastore.quality.QualityDataStore;
 import com.valsoft.cardiodiary.di.quality.QualityComponent;
-import com.valsoft.cardiodiary.presentation.ui.quality.QualityService;
 import com.valsoft.cardiodiary.presentation.viewmodel.quality.QualityListViewModel;
 import com.valsoft.cardiodiary.presentation.viewmodel.quality.QualityViewModel;
 
@@ -14,5 +13,4 @@ public interface GetQualityComponent {
     QualityDataStore qualityDataStore();
     void inject(QualityListViewModel qualityListViewModel);
     void inject(QualityViewModel qualityViewModel);
-    void inject(QualityService qualityService);
 }
